32e23085bd97888756cd4c45707515dcd12dc849
Benedek Vass <vabe44@gmail.com>
2018-04-28 16:29:18 +0200

    added recipe form

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'page-edit-recipe',
   templateUrl: 'edit-recipe.html',
 })
-export class EditRecipePage {
+export class EditRecipePage implements OnInit {
+  mode = 'New';
+  selectOptions = ['Easy', 'Medium', 'Hard'];
+  recipeForm: FormGroup;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EditRecipePage');
+  constructor(private navParams: NavParams) {}
+
+  ngOnInit() {
+    this.mode = this.navParams.get('mode');
+    this.initializeForm();
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
+  private initializeForm() {
+    this.recipeForm = new FormGroup({
+      'title': new FormControl(null, Validators.required),
+      'description': new FormControl(null, Validators.required),
+      'difficulty': new FormControl('Medium', Validators.required)
+    });
   }
 
 }
