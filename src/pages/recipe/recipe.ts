ae60ff314de54e29c0018f9e561ca27ceab788e6
Benedek Vass <vabe44@gmail.com>
2018-04-29 03:04:59 +0200

    finished app basic features

--- src/pages/recipe/recipe.ts
+++ src/pages/recipe/recipe.ts
@@ -1,3 +1,5 @@
+import { RecipesService } from './../../services/recipes';
+import { ShoppingListService } from './../../services/shopping-list';
 import { EditRecipePage } from './../edit-recipe/edit-recipe';
 import { NavController, NavParams } from 'ionic-angular';
 import { Component } from '@angular/core';
@@ -11,7 +13,11 @@ export class RecipePage {
   recipe: Recipe;
   index: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private slService: ShoppingListService,
+    private recipesService: RecipesService) {}
 
   ngOnInit() {
     this.recipe = this.navParams.get('recipe');
@@ -20,5 +26,14 @@ export class RecipePage {
 
   onEditRecipe() {
     this.navCtrl.push(EditRecipePage, {mode: 'Edit', recipe: this.recipe, index: this.index});
+  }
+
+  onAddIngredients() {
+    this.slService.addItems(this.recipe.ingredients);
+  }
+
+  onDeleteRecipe() {
+    this.recipesService.removeRecipe(this.index);
+    this.navCtrl.popToRoot();
   }
 }
